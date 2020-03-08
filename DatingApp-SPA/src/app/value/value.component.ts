0c90a4e64bf1ec3e77c08079da20929bdfed7cc2
unknown <dduartec@unal.edu.co>
2020-03-08 17:08:29 -0500

    asdfa

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -15,12 +15,12 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
-  getValues(){
+  getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
     }, error => {
       console.log(error);
-    })
+    });
   }
 
 }
